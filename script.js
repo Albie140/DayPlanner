0908a174b1902455dc8878dba79b44eff5efcd4d
Alberte Laventure <alberte.laventure@gmail.com>
2020-09-11 09:51:12 -0400

    Updated script file

--- script.js
+++ script.js
@@ -10,17 +10,46 @@ var task = " "
 
 
 function saveTask(timeBlocks, task) {
-
-    var task = localStorage.getItem("task");
-    console.log(this);
-
     $(".saveBtn").on("click", function () {
-        localStorage.setItem(timeBlocks, task);
+
         console.log(this)
-    //     for (var i = 0; i < timeBlocks.length; i++) {
-
-
-    //  }
+        var newTask = $(this).siblings(".description").val();
+        console.log(newTask)
+        var saveTime = $(this).attr("id");
+        localStorage.setItem(saveTime, newTask);
+retrieveTask()
     })
 }
 saveTask()
+
+function retrieveTask() {
+    $("#9").val(localStorage.getItem("9amSave"));
+    $("#10").val(localStorage.getItem("10amSave"));
+    $("#11").val(localStorage.getItem("11amSave"));
+    $("#12").val(localStorage.getItem("12pmSave"));
+    $("#13").val(localStorage.getItem("1pmSave"));
+    $("#14").val(localStorage.getItem("2pmSave"));
+    $("#15").val(localStorage.getItem("3pmSave"));
+    $("#16").val(localStorage.getItem("4pmSave"));
+    $("#17").val(localStorage.getItem("5pmSave"));
+
+}
+retrieveTask()
+
+//color coded function
+$(".description").each(function () {
+    var blockTime = JSON.parse($(this).attr("id"));
+    console.log(blockTime);
+    var currentTime = today.hours();
+    console.log(currentTime);
+    if (blockTime === currentTime) {
+        $(this).addClass("present")
+    }
+    if (blockTime < currentTime) {
+        $(this).addClass("past")
+    }
+    if (blockTime > currentTime) {
+        $(this).addClass("future")
+    }
+
+})
